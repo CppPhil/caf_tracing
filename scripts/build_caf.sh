074969d897bd177f0a2c5c0b7f69419dd8619dcd
CppPhil <CppPhil@users.noreply.github.com>
2020-04-10 17:18:02 +0200

    Initial Commit

---
+++ scripts/build_caf.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Directory containing this bash script
+readonly DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+readonly PREVIOUS_DIRECTORY=$(pwd)
+
+readonly ROOT_DIR=$DIR/..
+
+readonly CAF_BUILD_DIR=$ROOT_DIR/build/caf
+
+cd $ROOT_DIR/external
+
+cd ./actor-framework
+
+CC="ccache gcc" CXX="ccache g++" ./configure --build-dir=$CAF_BUILD_DIR --build-type=Debug --no-examples --no-python --with-log-level=TRACE --with-actor-profiler --with-runtime-checks
+cd $CAF_BUILD_DIR
+CC="ccache gcc" CXX="ccache g++" make -j$(nproc)
+
+cd $PREVIOUS_DIRECTORY
+
+exit 0
+
