26bc1c3759ca9e0800d39049cded45a7fa85a6c4
CppPhil <CppPhil@users.noreply.github.com>
2020-04-12 22:52:31 +0200

    Build CAF in Release mode

--- scripts/build_caf.sh
+++ scripts/build_caf.sh
@@ -13,7 +13,7 @@ cd $ROOT_DIR/external
 
 cd ./actor-framework-0.17.4
 
-CC="ccache gcc" CXX="ccache g++" ./configure --build-dir=$CAF_BUILD_DIR --build-type=Debug --no-examples --no-python --with-log-level=TRACE --with-actor-profiler --with-runtime-checks
+CC="ccache gcc" CXX="ccache g++" ./configure --build-dir=$CAF_BUILD_DIR --build-type=Release --no-examples --no-python --with-log-level=TRACE --with-actor-profiler --with-runtime-checks
 cd $CAF_BUILD_DIR
 CC="ccache gcc" CXX="ccache g++" make -j$(nproc)
 
