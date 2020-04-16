0e5cd9a79faef485fe58171e47517bbb2355e31b
CppPhil <CppPhil@users.noreply.github.com>
2020-04-16 15:54:10 +0200

    Add weird Inspector crap to span_context type; probably won't work.

--- shared/include/span_context.hpp
+++ shared/include/span_context.hpp
@@ -1,5 +1,10 @@
 #pragma once
 #include <string>
+#include <type_traits>
+
+#include <gsl/gsl_util>
+
+#include <caf/all.hpp>
 
 #include <opentracing/tracer.h>
 
@@ -16,6 +21,22 @@ public:
 
   tl::expected<std::unique_ptr<opentracing::SpanContext>, error>
   extract() const;
+
+  template <class Inspector>
+  friend std::enable_if_t<Inspector::reads_state,
+                          typename Inspector::result_type>
+  inspect(Inspector& f, const span_context& span_ctx) {
+    return f(caf::meta::type_name("span_context", span_ctx.buffer_));
+  }
+
+  template <class Inspector>
+  friend std::enable_if_t<Inspector::writes_state,
+                          typename Inspector::result_type>
+  inspect(Inspector& f, span_context& span_ctx) {
+    std::string string;
+    auto g = gsl::finally([&] { span_ctx.buffer_ = string; });
+    return f(caf::meta::type_name("span_context", string));
+  }
 
 private:
   explicit span_context(std::string&& serialized_span_context);
