f4797214fa8a8ad8948bb71143eef4ecef9d33f6
CppPhil <CppPhil@users.noreply.github.com>
2020-04-16 15:11:55 +0200

    Log whether spans are created with parents

--- shared/src/create_span.cpp
+++ shared/src/create_span.cpp
@@ -1,3 +1,7 @@
+#include <cstdio>
+
+#include <fmt/format.h>
+
 #include <opentracing/tracer.h>
 
 #include "create_span.hpp"
@@ -9,9 +13,12 @@ create_span(const std::string& serialized_span_context,
             const std::string& operation_name) {
   const auto ctx = extract(serialized_span_context);
 
-  if (!ctx.has_value() || *ctx == nullptr)
+  if (!ctx.has_value() || *ctx == nullptr) {
+    fmt::print(stderr, "Span \"{}\" has no parent!\n", operation_name);
     return opentracing::Tracer::Global()->StartSpan(operation_name);
+  }
 
+  fmt::print("Span \"{}\" created with parent!\n", operation_name);
   return opentracing::Tracer::Global()->StartSpan(
     operation_name, {opentracing::ChildOf(ctx->get())});
 }
