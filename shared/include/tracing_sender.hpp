9cf4d68be54693fa346aa77c72f994822bac9377
CppPhil <CppPhil@users.noreply.github.com>
2020-04-19 21:56:34 +0200

    Attempt to implement sending and receiving.
    Still broken.

--- shared/include/tracing_sender.hpp
+++ shared/include/tracing_sender.hpp
@@ -1,6 +1,10 @@
 #pragma once
 #include <utility>
 
+#include <caf/all.hpp>
+
+#include <pl/meta/remove_cvref.hpp>
+
 #include "message.hpp"
 
 namespace shared {
@@ -75,21 +79,104 @@ public:
   }
 
   // TODO: Consider making these so that they're called using operator->
-  template <class... Ts>
-  decltype(auto) send(Ts&&... xs) {
-    return self_->send(
-      message(span_context::inject(span_).value_or((shared::span_context())),
-              std::forward<Ts>(xs)...));
+  template <caf::message_priority P = caf::message_priority::normal,
+            class... Ts>
+  void send(const caf::group& dest, Ts&&... xs) {
+    self_->send(dest, make_message(std::forward<Ts>(xs)...));
+  }
+
+  template <caf::message_priority P = caf::message_priority::normal, class Dest,
+            class... Ts>
+  void send(const Dest& dest, Ts&&... xs) {
+    self_->send(dest, make_message(std::forward<Ts>(xs)...));
+  }
+
+  template <caf::message_priority P = caf::message_priority::normal,
+            class Dest = caf::actor, class... Ts>
+  void anon_send(const Dest& dest, Ts&&... xs) {
+    self_->anon_send(dest, make_message(std::forward<Ts>(xs)...));
+  }
+
+  template <caf::message_priority P = caf::message_priority::normal,
+            class Dest = caf::actor, class... Ts>
+  void scheduled_send(const Dest& dest, caf::actor_clock::time_point timeout,
+                      Ts&&... xs) {
+    self_->scheduled_send(dest, std::move(timeout),
+                          make_message(std::forward<Ts>(xs)...));
   }
 
   template <class... Ts>
-  decltype(auto) request(Ts&&... xs) {
-    return self_->request(
-      message(span_context::inject(span_).value_or((shared::span_context())),
-              std::forward<Ts>(xs)...));
+  void scheduled_send(const caf::group& dest,
+                      caf::actor_clock::time_point timeout, Ts&&... xs) {
+    self_->scheduled_send(dest, std::move(timeout),
+                          make_message(std::forward<Ts>(xs)...));
+  }
+
+  template <caf::message_priority P = caf::message_priority::normal,
+            class Rep = int, class Period = std::ratio<1>,
+            class Dest = caf::actor, class... Ts>
+  void delayed_send(const Dest& dest,
+                    std::chrono::duration<Rep, Period> rel_timeout,
+                    Ts&&... xs) {
+    self_->delayed_send(dest, std::move(rel_timeout),
+                        make_message(std::forward<Ts>(xs)...));
+  }
+
+  template <class Rep = int, class Period = std::ratio<1>,
+            class Dest = caf::actor, class... Ts>
+  void delayed_send(const caf::group& dest,
+                    std::chrono::duration<Rep, Period> rtime, Ts&&... xs) {
+    self_->delayed_send(dest, std::move(rtime),
+                        make_message(std::forward<Ts>(xs)...));
+  }
+
+  template <caf::message_priority P = caf::message_priority::normal,
+            class Dest = caf::actor, class... Ts>
+  void scheduled_anon_send(const Dest& dest,
+                           caf::actor_clock::time_point timeout, Ts&&... xs) {
+    self_->scheduled_anon_send(dest, std::move(timeout),
+                               make_message(std::forward<Ts>(xs)...));
+  }
+
+  template <caf::message_priority P = caf::message_priority::normal,
+            class Dest = caf::actor, class Rep = int,
+            class Period = std::ratio<1>, class... Ts>
+  void delayed_anon_send(const Dest& dest,
+                         std::chrono::duration<Rep, Period> rel_timeout,
+                         Ts&&... xs) {
+    self_->delayed_anon_send(dest, std::move(rel_timeout),
+                             make_message(std::forward<Ts>(xs)...));
+  }
+
+  template <class Rep = int, class Period = std::ratio<1>, class... Ts>
+  void delayed_anon_send(const caf::group& dest,
+                         std::chrono::duration<Rep, Period> rtime, Ts&&... xs) {
+    self_->delayed_anon_send(dest, std::move(rtime),
+                             make_message(std::forward<Ts>(xs)...));
+  }
+
+  template <caf::message_priority P = caf::message_priority::normal,
+            class Handle = caf::actor, class... Ts>
+  auto request(const Handle& dest, const caf::duration& timeout, Ts&&... xs) {
+    return self_->request(dest, timeout, make_message(std::forward<Ts>(xs)...));
+  }
+
+  template <caf::message_priority P = caf::message_priority::normal,
+            class Rep = int, class Period = std::ratio<1>,
+            class Handle = caf::actor, class... Ts>
+  auto request(const Handle& dest, std::chrono::duration<Rep, Period> timeout,
+               Ts&&... xs) {
+    return self_->request(dest, std::move(timeout),
+                          make_message(std::forward<Ts>(xs)...));
   }
 
 private:
+  template <class... Ts>
+  message<pl::meta::remove_cvref_t<Ts>...> make_message(Ts&&... xs) {
+    return message(span_context::inject(span_).value_or((span_context())),
+                   std::forward<Ts>(xs)...);
+  }
+
   T self_;
   std::unique_ptr<opentracing::Span> span_;
 };
