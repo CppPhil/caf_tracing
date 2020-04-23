3ed421fb2e77e14c826ff08064469d76c592a606
CppPhil <CppPhil@users.noreply.github.com>
2020-04-23 23:52:18 +0200

    client

--- client/src/client_actor.cpp
+++ client/src/client_actor.cpp
@@ -1,8 +1,10 @@
 #include <algorithm>
 
+#include "actor_profiler.hpp"
 #include "aprint.hpp"
 #include "client_actor.hpp"
 #include "create_span.hpp"
+#include "get_tracing_data.hpp"
 #include "tracer/get.hpp"
 #include "tracer/put.hpp"
 #include "types.hpp"
@@ -15,46 +17,74 @@ client_actor(shared::client_actor_type::pointer self,
 
   return {
     [self, remote_actor](caf::join_atom atom, std::string nickname) {
-      //      shared::tracing_sender self{self_, "join (client)"};
-      //      self.SetTag("nickname", nickname);
+      auto span = shared::create_span(shared::tracer::get(self->id()),
+                                      shared::get_tracing_data(self), "join");
+
+      span->SetTag("nickname", nickname);
+
+      shared::set_span_context(
+        shared::span_context::inject(shared::tracer::get(self->id()), span)
+          .value_or(shared::span_context{}));
 
       // Delegate join message to the server.
       self->send(remote_actor, atom, std::move(nickname), self);
     },
     [self, remote_actor](shared::ls_atom atom) {
-      // shared::tracing_sender self{self_, "ls (client)"};
+      auto span = shared::create_span(shared::tracer::get(self->id()),
+                                      shared::get_tracing_data(self), "ls");
+
+      shared::set_span_context(
+        shared::span_context::inject(shared::tracer::get(self->id()), span)
+          .value_or(shared::span_context{}));
 
       // Pass the /ls request to the server and properly print the result.
       self->request(remote_actor, caf::infinite, atom)
         .then([self](const std::vector<std::string>& vector) {
-          /*auto span = shared::create_span(message.span_ctx(),
-                                          "ls recv (client)");
-*/
+          auto span = shared::create_span(shared::tracer::get(self->id()),
+                                          shared::get_tracing_data(self),
+                                          "ls result");
+
           const auto to_print = fmt::format("Participants:\n[\n{}\n]\n",
                                             caf::join(vector, ",\n"));
 
-          // span->SetTag("participants", to_print);
+          span->SetTag("participants", to_print);
 
           shared::aprint(self, to_print);
         });
     },
     [self, remote_actor](caf::leave_atom atom, std::string goodbye_message) {
-      // shared::tracing_sender self{self_, "quit (client)"};
-      // self.SetTag("goodbye_message", goodbye_message);
+      auto span = shared::create_span(shared::tracer::get(self->id()),
+                                      shared::get_tracing_data(self), "quit");
+
+      span->SetTag("goodbye_message", goodbye_message);
+
+      shared::set_span_context(
+        shared::span_context::inject(shared::tracer::get(self->id()), span)
+          .value_or(shared::span_context{}));
 
       // Delegate the quit message to the server.
       self->send(remote_actor, atom, std::move(goodbye_message));
     },
     [self, remote_actor](shared::local_chat_atom, std::string message) {
-      // shared::tracing_sender self{self_, "chat (client)"};
-      // self.SetTag("message", message);
+      auto span = shared::create_span(shared::tracer::get(self->id()),
+                                      shared::get_tracing_data(self),
+                                      "local_chat");
+
+      span->SetTag("message", message);
+
+      shared::set_span_context(
+        shared::span_context::inject(shared::tracer::get(self->id()), span)
+          .value_or(shared::span_context{}));
 
       // Send any chat messages stemming from the CLI to the server.
       self->send(remote_actor, shared::chat_atom_v, std::move(message));
     },
     [self](shared::chat_atom, const std::string& message) {
-      // auto span = shared::create_span(msg.span_ctx(), "chat recv (client)");
-      // span->SetTag("message", message);
+      auto span = shared::create_span(shared::tracer::get(self->id()),
+                                      shared::get_tracing_data(self),
+                                      "receive chat");
+
+      span->SetTag("message", message);
 
       // Print any chat messages received from the remote server actor.
       shared::aprint(self, message);
@@ -62,8 +92,11 @@ client_actor(shared::client_actor_type::pointer self,
     [self,
      remote_actor](shared::ls_query_atom,
                    const std::string& client_nickname) -> caf::result<bool> {
-      // shared::tracing_sender self{self_, "ls_query (client)"};
-      // self.SetTag("client_nickname", client_nickname);
+      const auto* tracer = shared::tracer::get(self->id());
+      auto* trcing_data = shared::get_tracing_data(self);
+      auto span = shared::create_span(tracer, trcing_data, "ls_query");
+
+      span->SetTag("client_nickname", client_nickname);
 
       // Send an /ls request to the server and return whether the resulting
       // client list contains this local client.
@@ -71,15 +104,17 @@ client_actor(shared::client_actor_type::pointer self,
       // server side when performing an orderly shutdown.
       auto response_promise = self->make_response_promise<bool>();
 
+      shared::set_span_context(shared::span_context::inject(tracer, span)
+                                 .value_or(shared::span_context{}));
+
       self->request(remote_actor, caf::infinite, shared::ls_atom_v)
-        .then([client_nickname, response_promise](
+        .then([tracer, trcing_data, client_nickname, response_promise](
                 const std::vector<std::string>& result) mutable {
-          /*auto span = shared::create_span(message.span_ctx(),
-                                          "ls query recv (client)");
+          auto span = shared::create_span(tracer, trcing_data,
+                                          "receive ls_query result");
           span->SetTag("client_nickname", client_nickname);
           span->SetTag("result", fmt::format("Participants:\n[\n{}\n]\n",
-                                             caf::join(result, ",\n")));*/
-
+                                             caf::join(result, ",\n")));
           response_promise.deliver(
             std::find(result.begin(), result.end(), client_nickname)
             != result.end());
