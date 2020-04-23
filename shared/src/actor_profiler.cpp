5211c526eab02be3b0598a8c7e2fa2e751b1c4ca
CppPhil <CppPhil@users.noreply.github.com>
2020-04-24 00:04:23 +0200

    profiler

--- shared/src/actor_profiler.cpp
+++ shared/src/actor_profiler.cpp
@@ -1,4 +1,16 @@
+#include <cstdio>
+
+#include <utility>
+
+#include <fmt/format.h>
+
+#include <pl/current_function.hpp>
+
 #include "actor_profiler.hpp"
+#include "create_span.hpp"
+#include "span_context.hpp"
+#include "tracer/get.hpp"
+#include "tracing_data.hpp"
 
 namespace shared {
 namespace {
@@ -28,8 +40,15 @@ void actor_profiler::remove_actor([
 
 void actor_profiler::before_processing(const caf::local_actor& actor,
                                        const caf::mailbox_element& element) {
-#warning "TODO: HERE"
-  // TODO: HERE
+  auto* tracer = tracer::get(actor.id());
+
+  if (tracer == nullptr)
+    return;
+
+  auto span = create_span(tracer, element.tracing_id.get(),
+                          "before_processing");
+  span->SetTag("actor", actor.name());
+  span->SetTag("element", caf::to_string(element.content()));
 }
 
 void actor_profiler::after_processing(
@@ -40,14 +59,54 @@ void actor_profiler::after_processing(
 
 void actor_profiler::before_sending(const caf::local_actor& actor,
                                     caf::mailbox_element& element) {
-#warning "TODO: HERE"
-  // TODO: HERE
+  auto* tracer = tracer::get(actor.id());
+
+  if (tracer == nullptr)
+    return;
+
+  if (element.tracing_id == nullptr)
+    element.tracing_id = std::make_unique<tracing_data>(current_span_context);
+
+  auto span = create_span(tracer, element.tracing_id.get(), "before_sending");
+
+  span->SetTag("actor", actor.name());
+  span->SetTag("element", caf::to_string(element.content()));
+
+  const auto inject_res = span_context::inject(tracer, span);
+
+  if (!inject_res.has_value()) {
+    fmt::print(stderr, "{}: inject failed!\n", PL_CURRENT_FUNCTION);
+    return;
+  }
+
+  element.tracing_id = std::make_unique<tracing_data>(*inject_res);
 }
 
 void actor_profiler::before_sending_scheduled(
   const caf::local_actor& actor, caf::actor_clock::time_point timeout,
   caf::mailbox_element& element) {
-#warning "TODO: HERE"
-  // TODO: HERE
+  auto* tracer = tracer::get(actor.id());
+
+  if (tracer == nullptr)
+    return;
+
+  if (element.tracing_id == nullptr)
+    element.tracing_id = std::make_unique<tracing_data>(current_span_context);
+
+  auto span = create_span(tracer, element.tracing_id.get(),
+                          "before_sending_scheduled");
+
+  span->SetTag("actor", actor.name());
+  span->SetTag("timeout", timeout.time_since_epoch().count());
+  span->SetTag("element", caf::to_string(element.content()));
+
+  const auto inject_res = span_context::inject(tracer, span);
+
+  if (!inject_res.has_value()) {
+    fmt::print(stderr, "{}: inject failed!\n", PL_CURRENT_FUNCTION);
+    return;
+  }
+
+  element.tracing_id = std::make_unique<tracing_data>(*inject_res);
 }
 } // namespace shared
