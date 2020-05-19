e38cd486f75f80bfcb62d0e3cc7e5f8288467fd4
CppPhil <CppPhil@users.noreply.github.com>
2020-05-19 19:02:13 +0200

    Fix CAF

--- scripts/build_caf.sh
+++ scripts/build_caf.sh
@@ -7,14 +7,12 @@ readonly PREVIOUS_DIRECTORY=$(pwd)
 
 readonly ROOT_DIR=$DIR/..
 
-readonly CAF_BUILD_DIR=$ROOT_DIR/build/caf
-
 cd $ROOT_DIR/external
 
 cd ./actor-framework-0.17.4
 
-CC="ccache gcc" CXX="ccache g++" ./configure --build-dir=$CAF_BUILD_DIR --build-type=Release --no-examples --no-python --with-log-level=TRACE --with-actor-profiler --with-runtime-checks
-cd $CAF_BUILD_DIR
+CC="ccache gcc" CXX="ccache g++" ./configure --build-type=Release --no-examples --no-python --with-log-level=TRACE --with-actor-profiler --with-runtime-checks
+cd build 
 CC="ccache gcc" CXX="ccache g++" make -j$(nproc)
 
 cd $PREVIOUS_DIRECTORY
